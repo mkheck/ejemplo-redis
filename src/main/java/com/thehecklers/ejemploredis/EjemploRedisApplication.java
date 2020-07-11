4e5dab702145b60c93910b694d925793003bc4b3
Mark Heckler <mark.heckler@gmail.com>
2020-07-11 13:23:26 -0500

    Limpiar el soporte de repositorios

--- src/main/java/com/thehecklers/ejemploredis/EjemploRedisApplication.java
+++ src/main/java/com/thehecklers/ejemploredis/EjemploRedisApplication.java
@@ -18,43 +18,36 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 @EnableRedisRepositories
 public class EjemploRedisApplication {
-    @Bean
-    public RedisOperations<String, Aircraft> redisOperations(RedisConnectionFactory factory) {
-        Jackson2JsonRedisSerializer<Aircraft> serializer = new Jackson2JsonRedisSerializer<>(Aircraft.class);
-
-        RedisTemplate<String, Aircraft> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-        template.setDefaultSerializer(serializer);
-        template.setKeySerializer(new StringRedisSerializer());
-        System.out.println("Default serializer? " + template.isEnableDefaultSerializer());
-
-        return template;
-    }
+//    @Bean
+//    public RedisOperations<String, Aircraft> redisOperations(RedisConnectionFactory factory) {
+//        Jackson2JsonRedisSerializer<Aircraft> serializer = new Jackson2JsonRedisSerializer<>(Aircraft.class);
+//
+//        RedisTemplate<String, Aircraft> template = new RedisTemplate<>();
+//        template.setConnectionFactory(factory);
+//        template.setDefaultSerializer(serializer);
+//        template.setKeySerializer(new StringRedisSerializer());
+//        System.out.println("Default serializer? " + template.isEnableDefaultSerializer());
+//
+//        return template;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(EjemploRedisApplication.class, args);
     }
 }
 
-/*@EnableScheduling
+@EnableScheduling
 @Component
 class PollPlaneFinder {
     private WebClient client = WebClient.create("http://localhost:8080");
 
     private final RedisConnectionFactory connectionFactory;
-//    private final RedisConnection connection;
-//    private final RedisTemplate<String, String> template;
     private final AircraftRepository repository;
 
-//    PollPlaneFinder(RedisConnectionFactory connectionFactory, RedisTemplate<String, String> template, AircraftRepository repository) {
     PollPlaneFinder(RedisConnectionFactory connectionFactory, AircraftRepository repository) {
         this.connectionFactory = connectionFactory;
         this.repository = repository;
 
-//        this.connection = connectionFactory.getConnection();
-//        this.template = template;
-
-//        connection.serverCommands().flushDb();
         connectionFactory.getConnection().serverCommands().flushDb();
     }
 
@@ -69,40 +62,40 @@ class PollPlaneFinder {
 
         repository.findAll().forEach(System.out::println);
     }
-}*/
+}
 
 // MH: Toggle (comment out/in) above PollPlaneFinder class & this one to use repos or templates
 // MH: Yes, I know it's crude, it's a QnD WIP ;)
-@EnableScheduling
-@Component
-class PollPlaneFinder {
-    private WebClient client = WebClient.create("http://localhost:8080");
-
-    private final RedisConnectionFactory connectionFactory;
-    private final RedisConnection connection;
-    //private final RedisTemplate<String, String> template;
-    private final RedisOperations<String, Aircraft> redisOperations;
-
-    PollPlaneFinder(RedisConnectionFactory connectionFactory, RedisOperations<String, Aircraft> redisOperations) {
-        this.connectionFactory = connectionFactory;
-        this.connection = connectionFactory.getConnection();
-        this.redisOperations = redisOperations;
-
-        connection.serverCommands().flushDb();
-    }
-
-    @Scheduled(fixedRate = 1000)
-    private void pollPlanes() {
-        client.get()
-                .retrieve()
-                .bodyToFlux(Aircraft.class)
-                .filter(plane -> !plane.getReg().isEmpty())
-                .toStream()
-                .forEach(ac -> redisOperations.opsForValue().set(ac.getReg(), ac));
-
-        redisOperations.opsForValue()
-                .getOperations()
-                .keys("*")
-                .forEach(ac -> System.out.println(redisOperations.opsForValue().get(ac)));
-    }
-}
+//@EnableScheduling
+//@Component
+//class PollPlaneFinder {
+//    private WebClient client = WebClient.create("http://localhost:8080");
+//
+//    private final RedisConnectionFactory connectionFactory;
+//    private final RedisConnection connection;
+//    //private final RedisTemplate<String, String> template;
+//    private final RedisOperations<String, Aircraft> redisOperations;
+//
+//    PollPlaneFinder(RedisConnectionFactory connectionFactory, RedisOperations<String, Aircraft> redisOperations) {
+//        this.connectionFactory = connectionFactory;
+//        this.connection = connectionFactory.getConnection();
+//        this.redisOperations = redisOperations;
+//
+//        connection.serverCommands().flushDb();
+//    }
+//
+//    @Scheduled(fixedRate = 1000)
+//    private void pollPlanes() {
+//        client.get()
+//                .retrieve()
+//                .bodyToFlux(Aircraft.class)
+//                .filter(plane -> !plane.getReg().isEmpty())
+//                .toStream()
+//                .forEach(ac -> redisOperations.opsForValue().set(ac.getReg(), ac));
+//
+//        redisOperations.opsForValue()
+//                .getOperations()
+//                .keys("*")
+//                .forEach(ac -> System.out.println(redisOperations.opsForValue().get(ac)));
+//    }
+//}
